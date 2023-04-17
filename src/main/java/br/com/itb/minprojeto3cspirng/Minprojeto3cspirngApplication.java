bb8b209b6e808ab307a8dd41bcd00bed7e8a434a
pedroalmeida.9188@gmail.com <pedroalmeida.9188>
2023-04-17 10:19:32 -0300

    primeiro commit

---
+++ src/main/java/br/com/itb/minprojeto3cspirng/Minprojeto3cspirngApplication.java
@@ -0,0 +1,13 @@
+package br.com.itb.minprojeto3cspirng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Minprojeto3cspirngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Minprojeto3cspirngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itb/minprojeto3cspirng/Minprojeto3cspirngApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itb.minprojeto3cspirng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Minprojeto3cspirngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
